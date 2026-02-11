50ea03274b1ef643927c4a4188aa1310f864ef73
nidhihk003 <shpnidhi03@gmail.com>
2026-02-11 12:56:30 +0530

    Add generated test: test_banking_test.py

--- Banking App/test_banking_test.py
+++ Banking App/test_banking_test.py
@@ -1,29 +1,29 @@
-def test_create_account_valid_output():
-    assert card.create_account() == None
+def test_create_account_generates_valid_card_number():
+    assert Card().create_account() == None
 
-def test_log_in_successful_login():
-    assert card.log_in() == None
+def test_log_in_with_valid_credentials():
+    assert Card().log_in() == None
 
-def test_log_in_invalid_card():
-    assert card.log_in() == None
+def test_log_in_with_invalid_credentials():
+    assert Card().log_in() == None
 
-def test_log_in_invalid_pin():
-    assert card.log_in() == None
-
-def test_success_balance_display():
-    assert card.success() == None
+def test_success_view_balance():
+    assert Card().success() == None
 
 def test_success_add_income():
-    assert card.success() == None
+    assert Card().success() == None
 
 def test_success_do_transfer():
-    assert card.success() == None
+    assert Card().success() == None
+
+def test_success_do_transfer_insufficient_funds():
+    assert Card().success() == None
 
 def test_success_close_account():
-    assert card.success() == None
-
-def test_success_log_out():
-    assert card.success() == None
+    assert Card().success() == None
 
 def test_luhn_valid_card_number():
-    assert card.luhn('4000001234567890') == 4000001234567890
+    assert Card().luhn_2('4000001234567890') == True
+
+def test_luhn_invalid_card_number():
+    assert Card().luhn_2('1234567890123456') == False
