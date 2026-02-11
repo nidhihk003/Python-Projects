1b426ec7aa272bd80c3ee64f3708f9f089daddb5
nidhihk003 <shpnidhi03@gmail.com>
2026-02-11 11:25:05 +0530

    Add generated test: test_main_test.py

--- Car Racing Game/test_main_test.py
+++ Car Racing Game/test_main_test.py
@@ -1,29 +1,29 @@
 def test_car_initial_position():
-    assert Car().x == 460
+    assert Car().x, Car().y == [460, 530]
 
 def test_car_move_left_within_bounds():
-    assert car = Car(); car.move_left(); car.x == 455
+    assert car = Car(); car.move_left(); car.update_position(); car.x == 455
 
 def test_car_move_right_within_bounds():
-    assert car = Car(); car.move_right(); car.x == 465
+    assert car = Car(); car.move_right(); car.update_position(); car.x == 465
 
 def test_car_move_forward():
-    assert car = Car(); car.move_forward(); car.speed_y == -5
+    assert car = Car(); car.move_forward(); car.update_position(); car.y == 525
 
 def test_car_move_backward():
-    assert car = Car(); car.move_backward(); car.speed_y == 5
+    assert car = Car(); car.move_backward(); car.update_position(); car.y == 535
+
+def test_car_stop_x():
+    assert car = Car(); car.stop_x(); car.speed_x == 0
+
+def test_car_stop_y():
+    assert car = Car(); car.stop_y(); car.speed_y == 0
 
 def test_road_initial_position():
     assert Road().y == 0
 
-def test_road_move_down():
+def test_road_move_within_bounds():
     assert road = Road(); road.move(); road.y == 5
 
-def test_hurdle_manager_create_hurdle():
-    assert hurdle_manager = HurdleManager(); hurdle_manager.create_hurdle(); len(hurdle_manager.hurdles) == 1
-
-def test_hurdle_manager_move_hurdles():
-    assert hurdle_manager = HurdleManager(); hurdle_manager.create_hurdle(); hurdle_manager.move_hurdles(); hurdle_manager.hurdles[0].y == -15
-
-def test_update_score():
-    assert game = Game(); game.update_score(); game.score == 1
+def test_create_hurdle():
+    assert manager = HurdleManager(); manager.create_hurdle(); len(manager.hurdles) == 1
