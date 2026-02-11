3e30c927d2098bc234f558d7f82ebdb3065d9b21
nidhihk003 <shpnidhi03@gmail.com>
2026-02-11 12:56:53 +0530

    Add generated test: test_game_of_life_test.py

--- Banking App/test_game_of_life_test.py
+++ Banking App/test_game_of_life_test.py
@@ -1,29 +1,29 @@
-def test_update_empty_grid_no_progress():
+def test_update_empty_grid():
     assert update(pygame.Surface((800, 600)), np.zeros((5, 5)), 10) == np.zeros((5, 5))
 
-def test_update_single_alive_cell_no_progress():
+def test_update_single_cell():
     assert update(pygame.Surface((800, 600)), np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]), 10) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
-def test_update_block_pattern_no_progress():
-    assert update(pygame.Surface((800, 600)), np.array([[0, 1, 1], [0, 1, 1], [0, 0, 0]]), 10) == np.array([[0, 1, 1], [0, 1, 1], [0, 0, 0]])
+def test_update_underpopulation():
+    assert update(pygame.Surface((800, 600)), np.array([[1, 0, 0], [0, 1, 0], [0, 0, 0]]), 10) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
-def test_update_blinker_pattern_no_progress():
+def test_update_overpopulation():
+    assert update(pygame.Surface((800, 600)), np.array([[1, 1, 1], [1, 1, 0], [0, 0, 0]]), 10) == np.array([[1, 0, 1], [1, 0, 0], [0, 0, 0]])
+
+def test_update_survival():
+    assert update(pygame.Surface((800, 600)), np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]]), 10) == np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]])
+
+def test_update_reproduction():
+    assert update(pygame.Surface((800, 600)), np.array([[0, 1, 0], [1, 1, 0], [0, 0, 0]]), 10) == np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]])
+
+def test_update_with_progress():
+    assert update(pygame.Surface((800, 600)), np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]), 10, with_progress=True) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+def test_update_still_life():
+    assert update(pygame.Surface((800, 600)), np.array([[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]), 10) == np.array([[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]])
+
+def test_update_oscillator():
     assert update(pygame.Surface((800, 600)), np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]]), 10) == np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
-
-def test_update_alive_cell_with_three_neighbors_no_progress():
-    assert update(pygame.Surface((800, 600)), np.array([[1, 1, 0], [1, 0, 0], [1, 0, 0]]), 10) == np.array([[1, 1, 0], [1, 1, 0], [0, 0, 0]])
-
-def test_update_all_cells_alive_no_progress():
-    assert update(pygame.Surface((800, 600)), np.ones((3, 3)), 10) == np.array([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
 
 def test_update_empty_grid_with_progress():
     assert update(pygame.Surface((800, 600)), np.zeros((5, 5)), 10, with_progress=True) == np.zeros((5, 5))
-
-def test_update_single_alive_cell_with_progress():
-    assert update(pygame.Surface((800, 600)), np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]]), 10, with_progress=True) == np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-
-def test_update_block_pattern_with_progress():
-    assert update(pygame.Surface((800, 600)), np.array([[0, 1, 1], [0, 1, 1], [0, 0, 0]]), 10, with_progress=True) == np.array([[0, 1, 1], [0, 1, 1], [0, 0, 0]])
-
-def test_update_blinker_pattern_with_progress():
-    assert update(pygame.Surface((800, 600)), np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]]), 10, with_progress=True) == np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
